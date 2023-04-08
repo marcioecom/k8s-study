fcfe5f397b0cc9b7cf42fceb43e80d2820fc4c92
Márcio Júnior <marcio.mj70@gmail.com>
2023-04-08 00:31:26 -0300

    go up kind

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/marcioecom/hello-go
+
+go 1.19
---
+++ server.go
@@ -0,0 +1,12 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":3000", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Hello World</h1>"))
+}
