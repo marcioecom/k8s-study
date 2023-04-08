55bc64375a70450312d81a628bc42af3b05b115d
Márcio Júnior <marcio.mj70@gmail.com>
2023-04-08 14:39:08 -0300

    config map with volume

--- server.go
+++ server.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/configmap", configMap)
 	http.ListenAndServe(":3000", nil)
 }
 
@@ -16,4 +18,13 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	age := os.Getenv("AGE")
 
 	fmt.Fprintf(w, "Hello, I'm %s and I'm %s years old", name, age)
+}
+
+func configMap(w http.ResponseWriter, r *http.Request) {
+	data, err := os.ReadFile("myskills/skills.txt")
+	if err != nil {
+		log.Fatalf("error reading file: %v", err)
+	}
+
+	fmt.Fprintf(w, "My skills: %s", string(data))
 }
